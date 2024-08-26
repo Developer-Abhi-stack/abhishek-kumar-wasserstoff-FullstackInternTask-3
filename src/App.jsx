bcf03609930bd8629642c8f9a5daef4b5241d51e
Abhishekkumar2001 <abhikum8131@gmail.com>
2024-08-26 16:19:19 +0530

    Created topbuttons and inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import Inputs from "./components/Inputs"
+import TopButtons from "./components/TopButtons"
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-5xl text-red-700">Hello World !</h1>
+    <div className="mx-auto max-w-screen-lg mt-4 py-5 px-32 bg-gradient-to-br shadow-lg shadow-gray-400 from-cyan-500 to-blue-500">
+      <TopButtons />
+      <Inputs />
     </div>
   )
 }
---
+++ src/components/TopButtons.jsx
@@ -0,0 +1,39 @@
+const TopButtons = () => {
+  const cities = [
+    {
+      id: 1,
+      name: "New Delhi",
+    },
+    {
+      id: 2,
+      name: "Mumbai",
+    },
+    {
+      id: 1,
+      name: "Chennai",
+    },
+    {
+      id: 1,
+      name: "Kolkata",
+    },
+    {
+      id: 1,
+      name: "Patna",
+    },
+  ];
+
+  return (
+    <div className="flex justify-around items-center my-6">
+      {cities.map((item) => (
+        <button
+          key={item.id}
+          className="text-lg font-medium hover:bg-gray-700/20 px-3 py-2 rounded-md transition ease-in"
+        >
+          {item.name}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default TopButtons;
